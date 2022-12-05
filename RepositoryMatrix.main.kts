b331334290c5d23040accebabb583a6500ceeb69
Xini1 <tereshchenko.xini@gmail.com>
2022-12-05 22:44:13 +0100

    fix output format

--- RepositoryMatrix.main.kts
+++ RepositoryMatrix.main.kts
@@ -2,6 +2,12 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun repositories(): Collection<String> = Files.readAllLines(Paths.get("repositories"))
+
+fun withDoubleQuotes(strings: Collection<String>) =
+    strings.asSequence()
+        .map { '"' + it + '"' }
+        .toList()
+
 fun json(repositories: Collection<String>) = """{"include":$repositories}"""
 
-println(json(repositories()))
+println(json(withDoubleQuotes(repositories())))
